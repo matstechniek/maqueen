bd31ecebdba87566e4a308746f4d35e44dcedbda
matstechniek <150784411+matstechniek@users.noreply.github.com>
2024-02-20 09:36:46 +0100

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+let ir_waarde = 0
+let vooruit = 10
+let achteruit = 27
+let links = 73
+let rechts = 68
 basic.forever(function () {
-	
+    basic.showNumber(ir_waarde)
+})
+basic.forever(function () {
+    ir_waarde = IR.IR_read()
 })
